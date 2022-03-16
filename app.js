0593530ad81b8a5e7a288efa82423aba045656c9
rabiulalam1 <rabiul.alam@outlook.com>
2022-03-15 22:11:30 -0400

    Done

--- app.js
+++ app.js
@@ -8,18 +8,51 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //C://desktop/ironbeer/views
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  // console.log(req.params.id)
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('random-beer', { beer });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
